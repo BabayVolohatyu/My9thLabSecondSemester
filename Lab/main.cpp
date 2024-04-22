9578dd48cdb8926b458ed4a73decba6c56d9335f
BabayVolohatyu <babayvolohatyu@gmail.com>
2024-04-23 00:08:09 +0300

    Third task

--- Lab/main.cpp
+++ Lab/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <deque>
 
 template<typename T, size_t S1, size_t S2>
 std::array<T,S1+S2> mergeArrays(const std::array<T,S1> &array1, const std::array<T,S2> &array2){
@@ -40,6 +41,16 @@ std::pair<std::vector<T>, std::vector<T>> splitVector(const std::vector<T> &vect
     }
     return newPair;
 }
+template<typename T>
+bool isPalindrome(const std::deque<T> &dq){
+    auto itFront = dq.begin();
+    auto itBack = dq.end();
+    itBack--;
+    for(;itFront!=dq.end();itFront++, itBack--){
+        if(*itFront!=*itBack) return false;
+    }
+    return true;
+}
 int main() {
     std::array<int, 5> intar1{0,2,7,9,15};
     std::array<int, 4> intar2{-1,0,1,10};
@@ -51,5 +62,15 @@ int main() {
     std::pair intPair = splitVector(intvec);
     std::vector<bool> boolvec{true, false, true};
     std::pair boolPair = splitVector(boolvec);
+    std::deque<int> intdq1{0,0,1,0,0};
+    std::deque<int> intdq2{0,0,0,0};
+    std::deque<char> chardq1{'l','o','l'};
+    std::deque<char> chardq2{'c','+','+'};
+    bool firstIntCheck = isPalindrome(intdq1);
+    bool secondIntCheck = isPalindrome(intdq2);
+    bool firstCharCheck = isPalindrome(chardq1);
+    bool secondCharCheck = isPalindrome(chardq2);
+
+
     return 0;
 }
